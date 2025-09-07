5ee34910bcde2df097967bb017bb811fc88c951b
Jack Sanders <personal@jacksanders.uk>
2025-09-07 16:38:06 +0100

    refactor: clean up shifting text code

--- app/components/shifting-text/ShiftingText.tsx
+++ app/components/shifting-text/ShiftingText.tsx
@@ -1,10 +1,94 @@
-import { useEffect, useRef} from 'react';
+import { useEffect, useRef } from 'react';
 import type { ReactNode, ReactElement } from 'react';
 import type { Route } from '../../+types/root';
 
+/**
+ * Returns a randomly-chosen integer from a range, excluding one
+ * @param exclude the number to exclude
+ * @param lower the lower bound of the range
+ * @param upper the upper bound of the range
+ * @returns a random number between `lower` and `upper`, which will not be `exclude`
+ */
+const randomRangeExcluding = (exclude: number, lower = 0, upper = 1): number => {
+  let initialRandom = Math.floor(lower + Math.random() * (upper - lower - 1));
+
+  // Ensure excluded number is not chosen
+  if (initialRandom >= exclude) {
+    initialRandom += 1;
+  }
+
+  return initialRandom;
+};
+
+// Config for shifting text
+const FONTS = [
+  'Aladin',
+  'Condiment',
+  'Gloria Hallelujah',
+  'Grandstander',
+  'Knewave',
+  'Permanent Marker',
+  'Potta One',
+  'Rubik Glitch',
+  'Ruslan Display',
+  'Slackey',
+];
+
+const THINGS = [
+  'stuff',
+  'things',
+  'websites',
+  'apps',
+  'databases',
+  'tools',
+  'experiences',
+  'solutions',
+];
+
+const DISPLAY_TIME = 3000;
+const TRANSITION_TIME = 500;
+
+/**
+ * Displays text which changes content and font every 3 seconds
+ */
+const ShiftingText = (): ReactElement => {
+  const textRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    let thingIndex = 0;
+
+    const intervalId = setInterval(() => {
+      if (!textRef.current) return;
+
+      textRef.current.style.opacity = '0';
+
+      setTimeout(() => {
+        if (!textRef.current) return;
+
+        const fontIndex = Math.floor(Math.random() * FONTS.length);
+        thingIndex = randomRangeExcluding(thingIndex, 0, THINGS.length);
+
+        textRef.current.style.fontFamily = FONTS[fontIndex];
+        textRef.current.textContent = THINGS[thingIndex];
+
+        textRef.current.style.opacity = '1';
+      }, TRANSITION_TIME); // Wait until opcacity transition completes before changing anything
+    }, DISPLAY_TIME);
+
+    // Clean up interval on unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <span ref={textRef} className={`transition-opacity duration-[${TRANSITION_TIME}ms]`}>
+      stuff
+    </span>
+  );
+};
+
+// Import required fonts from google fonts
 const links: Route.LinksFunction = () => [
   {
-    // Fonts from Google fonts
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2'
       + '?family=Aladin'
@@ -20,67 +104,6 @@ const links: Route.LinksFunction = () => [
       + '&display=swap',
   },
 ];
-
-const ShiftingText = ({
-  children,
-}: {
-  children: ReactNode;
-}): ReactElement => {
-  const textRef = useRef<HTMLSpanElement>(null);
-
-  useEffect(() => {
-    const fonts = [
-      'Aladin',
-      'Condiment',
-      'Gloria Hallelujah',
-      'Grandstander',
-      'Knewave',
-      'Permanent Marker',
-      'Potta One',
-      'Rubik Glitch',
-      'Ruslan Display',
-      'Slackey',
-    ];
-
-    const things = [
-      'stuff',
-      'things',
-      'websites',
-      'apps',
-      'databases',
-      'tools',
-      'experiences',
-      'solutions',
-    ];
-
-    const intervalId = setInterval(() => {
-      if (!textRef.current) return;
-
-      textRef.current.style.opacity = '0';
-
-      setTimeout(() => {
-        if (!textRef.current) return;
-
-        const fontIndex = Math.floor(Math.random() * fonts.length);
-        const thingIndex = Math.floor(Math.random() * things.length);
-
-        textRef.current.style.fontFamily = fonts[fontIndex];
-        textRef.current.textContent = things[thingIndex];
-
-        textRef.current.style.opacity = '1';
-      }, 500); // Wait until opcacity transition completes before changing anything
-    }, 3000);
-
-    // Clean up interval on unmount
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return (
-    <span ref={textRef} className='transition-opacity duration-500' style={{ fontFamily: 'Aladin' }}>
-      {children}
-    </span>
-  );
-};
 
 export default ShiftingText;
 export { links };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,7 +9,7 @@ const Home = (): ReactElement => (
     <div className="flex flex-col items-center justify-center h-screen ">
       <h1 className="text-7xl text-center flex flex-col gap-3 justify-between">
         I make
-        <ShiftingText>stuff</ShiftingText>
+        <ShiftingText />
       </h1>
     </div>
   </Fragment>
